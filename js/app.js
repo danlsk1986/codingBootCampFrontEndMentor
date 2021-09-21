eeecc56723e477dffebf3fbbabe5d645cccfb46f
Daniel Lasauskas <danlsk1986@gmail.com>
2021-09-21 17:23:02 -0300

    fix del tamaño

--- js/app.js
+++ js/app.js
@@ -14,16 +14,14 @@ const users = [
     {   img: 'images/image-tanya.jpg',
         name: 'Tanya Sinclair',
         profession: 'UX Engineer',
-        quote: `“ I’ve been interested in coding for a while but never taken the jump, until now. 
-        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+        quote: `“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
         excited about the future. ”`
     },
     {
         img: 'images/image-john.jpg',
         name:'John Takpor',
         profession:'Junior Front-end Developer',
-        quote:`“ If you want to lay the best foundation possible I’d recommend taking this course. 
-        The depth the instructors go into is incredible. I now feel so confident about 
+        quote:`“ If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible. I now feel so confident about 
         starting up as a professional developer. ”`
     }
 ]
